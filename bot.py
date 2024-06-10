08ce806cfffb84e43a416fa39fd89fba5d8a14b0
gaulerie <emilien.courvoisier@gmail.com>
2024-06-10 20:39:23 +0900

    Final

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
-import os  # Ajout de l'importation du module os
-from commands import register_commands  # Importer la fonction pour enregistrer les commandes
+import os
+import sys
+from commands import register_commands
 
 print("Starting bot...")  # Log initial
 
@@ -16,6 +17,34 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-register_commands(bot)  # Enregistrer les commandes
+# Verrouillage
+lock_file = "/tmp/discord_bot.lock"
 
-bot.run(DISCORD_TOKEN)
+def check_and_create_lock():
+    if os.path.exists(lock_file):
+        print("Another instance is already running. Exiting.")
+        sys.exit(1)
+    with open(lock_file, 'w') as f:
+        f.write(str(os.getpid()))
+
+def remove_lock():
+    if os.path.exists(lock_file):
+        os.remove(lock_file)
+
+check_and_create_lock()
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} has connected to Discord!')
+
+@bot.event
+async def on_disconnect():
+    remove_lock()
+
+# Enregistrer les commandes
+register_commands(bot)
+
+try:
+    bot.run(DISCORD_TOKEN)
+finally:
+    remove_lock()
