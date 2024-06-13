8477ed4515d2b08fdbe5800b16dd5d13dc429279
gaulerie <emilien.courvoisier@gmail.com>
2024-06-13 09:52:49 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ intents.message_content = True
 intents.guilds = True
 intents.members = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents, help_command=None)
 
 # Enregistrer les commandes
 register_commands(bot)
