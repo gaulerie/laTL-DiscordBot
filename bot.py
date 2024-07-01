52f7925765a187e6bcc9a7c4e464f6322cb3c7dc
gaulerie <emilien.courvoisier@gmail.com>
2024-07-01 15:46:08 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands, tasks
 import os
 from commands import register_commands, update_sheet, clean_verification_codes
-import random
 
 intents = discord.Intents.default()
 intents.messages = True
@@ -18,20 +17,7 @@ register_commands(bot)
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
-    apply_rainbow_roles.start()  # Démarrer la tâche de changement de couleur
     clean_verification_codes.start()
-
-@tasks.loop(seconds=5)  # Change de couleur toutes les 5000ms
-async def apply_rainbow_roles():
-    guild = bot.guilds[0]  # Suppose que le bot est uniquement dans votre serveur
-    if guild:
-        role = discord.utils.get(guild.roles, name="Admin")
-        if role:
-            random_color = discord.Color(random.randint(0, 0xFFFFFF))
-            await role.edit(color=random_color)
-            print(f'Changed role color to {random_color}')
-        else:
-            print("Role 'Admin' not found")
 
 @bot.event
 async def on_member_join(member):
